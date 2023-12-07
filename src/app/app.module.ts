14d0f22eb776f73b311e58797842c0e269de2483
aacgectyuoki <maxdell-thibodeau@hotmail.com>
2023-12-06 22:11:59 -0500

    next deployment ready

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,12 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import {MatRadioModule} from '@angular/material/radio';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
@@ -16,6 +20,21 @@ import { SponsorsComponent } from './sponsors/sponsors.component';
 import { TrainingComponent } from './training/training.component';
 import { RecordsComponent } from './records/records.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+// import { BREAKPOINT } from '@angular/flex-layout';
+
+// const CUSTOM_BREAKPOINTS = [
+//   {
+//     alias: 'sm',
+//     mediaQuery: 'screen and (min-width: 600px) and (max-width: 959px)',
+//   },
+//   {
+//     alias: 'gt-xs',
+//     overlapping: true,
+//     mediaQuery: 'screen and (min-width: 800px)'
+//   },
+//   // ... other custom breakpoints if needed ...
+// ];
 
 @NgModule({
   declarations: [
@@ -37,10 +56,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatSelectModule,
     MatInputModule,
     MatRadioModule,
+    MatListModule,
+    MatSidenavModule,
     MatCheckboxModule,
-    ReactiveFormsModule
+    MatToolbarModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    FlexLayoutModule
   ],
-  providers: [],
+  providers: [
+    // {
+    //   provide: BREAKPOINT,
+    //   useValue: CUSTOM_BREAKPOINTS,
+    //   multi: true
+    // }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
